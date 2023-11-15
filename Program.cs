26d2958794dd3c57cb53af987afda61fd82ff58e
Ushasree Kamala <ushasree210@gmail.com>
2023-11-15 11:41:46 -0600

    added somefiles to gitignore and the rest of files arenot

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variables_and_constants
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
